b5fe29979a64813c6d6e5283b472f58671b10bf4
denistred <denislylov2004@mail.ru>
2023-02-09 00:03:46 +0300

    drag&drop image change

--- source/pil_dragimage.py
+++ source/pil_dragimage.py
@@ -1,11 +1,15 @@
-from PIL import Image, ImageDraw, ImageFont, ImageQt
+from PIL import Image, ImageDraw, ImageFont, ImageQt, ImageFilter
+
 
 def create_image(text):
     if len(text) > 50:
         text = text[:50] + '...'
-    image = Image.new('RGBA', (355, 25))
+    image = Image.new('RGBA', (365, 35))
     im = ImageDraw.Draw(image)
-    im.rounded_rectangle(((0, 0), (355, 25)), 5, fill=(255, 255, 255))
-    font = ImageFont.truetype('fonts/arial.ttf', 13)
-    im.text((5, 7), text, fill=(0,0,0), font=font)
+    im.rectangle(((7, 7), (343, 28)), fill=(0, 0, 0, 100))
+    image = image.filter(ImageFilter.GaussianBlur(radius=3))
+    im = ImageDraw.Draw(image)
+    im.rounded_rectangle(((7, 7), (343, 28)), 3, fill=(255, 255, 255))
+    font = ImageFont.truetype('fonts/arial.ttf', 13, )
+    im.text((10, 10), text, fill=(0, 0, 0), font=font)
     return ImageQt.ImageQt(image)
